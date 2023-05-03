442024c376d19de6c102f5d89159f3080e6f5f56
Dushmanthr <dushmantha15ranathunga@gmail.com>
2023-05-03 21:12:58 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './app.css';
+
+import Init from './Init'
+
+
+
+
+
+const App = () => {
+    return (
+        <>
+         <Init/>
+        
+        </>
+    )
+}
+
+export default App
